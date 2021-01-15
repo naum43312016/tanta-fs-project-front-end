b84ee518ae05c623a37c8f370b8fc075787d3562
jsjow <joey.spijer@gmail.com>
2021-01-16 01:23:14 +0200

    home page again

--- src/Components/HomePage/Filter.jsx
+++ src/Components/HomePage/Filter.jsx
@@ -3,17 +3,17 @@ import { Input, Row } from 'reactstrap';
 
 const Filter = () => {
     return (
-        <Row style={{height:"100px", paddingTop:"40px", paddingBottom:"130px"}} className="align-items-center ml-4">
+        <Row style={{height:"100px", paddingTop:"40px", marginBottom:"115px"}} className="align-items-center ml-4">
             <div className="offset-sm-1 col-sm-2 mr-sm-5 col-3">
                 <label>Find by category :</label>
                 <Input type="select">
                     <option value="" selected>Choose here</option>
                 </Input>
             </div>
-            <button style={{ borderLeft: 'none' }} className="filter-btn col-2">Furniture</button>
+            <button className="filter-btn col-2">Furniture</button>
             <button className="filter-btn col-2">Kitchen</button>
             <button className="filter-btn col-2">Garden</button>
-            <button style={{ borderRight: 'none' }} className="filter-btn col-2">Electronics</button>
+            <button className="filter-btn col-2">Electronics</button>
         </Row>
     )
 }
--- src/Components/HomePage/ItemCard.jsx
+++ src/Components/HomePage/ItemCard.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Card, CardBody, CardTitle, CardSubtitle, CardText, Card
 const ItemCard = () => {
     return (
         <div style={{width:"80%", marginLeft:"10%"}} className="card-container mt-5">
-                <Card style={{margin:"0 10px 10px 10px"}} className="mb-3">
+                <Card>
                     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
                     <CardBody>
                         <CardTitle tag="h5">Card title</CardTitle>
@@ -12,7 +12,7 @@ const ItemCard = () => {
                         <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                     </CardBody>
                 </Card>
-                <Card style={{margin:"0 10px 10px 10px"}} className="mb-3">
+                <Card>
                     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
                     <CardBody>
                         <CardTitle tag="h5">Card title</CardTitle>
@@ -20,7 +20,7 @@ const ItemCard = () => {
                         <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                     </CardBody>
                 </Card>
-                <Card style={{margin:"0 10px 10px 10px"}} className="mb-3">
+                <Card>
                     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
                     <CardBody>
                         <CardTitle tag="h5">Card title</CardTitle>
@@ -28,7 +28,7 @@ const ItemCard = () => {
                         <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                     </CardBody>
                 </Card>
-                <Card style={{margin:"0 10px 10px 10px"}} className="mb-3">
+                <Card>
                     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
                     <CardBody>
                         <CardTitle tag="h5">Card title</CardTitle>
@@ -36,7 +36,15 @@ const ItemCard = () => {
                         <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                     </CardBody>
                 </Card>
-                <Card style={{margin:"0 10px 10px 10px"}} className="mb-3">
+                <Card>
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle tag="h5">Card title</CardTitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    </CardBody>
+                </Card>
+                <Card>
                     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
                     <CardBody>
                         <CardTitle tag="h5">Card title</CardTitle>
