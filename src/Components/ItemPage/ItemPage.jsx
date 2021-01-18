72f70ff6f00a91009d7aa01b4a5c9e17cde72904
Yoel Zerbib <yoelzerbib7@gmail.com>
2021-01-18 12:04:25 +0200

    resolved conflicts

--- src/Components/ItemPage/ItemPage.jsx
+++ src/Components/ItemPage/ItemPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Row, Container } from 'reactstrap';
 import ItemDetails from './ItemDetails';
 import axios from 'axios';
+import Fox from '../HomePage/971c17c93f74bc7280e285153b2e1ace-700.jpg';
+import {BASE_URL} from '../HomePage/Home'
 
 const ItemPage = () => {
     const [item, setItem] = useState([]);
